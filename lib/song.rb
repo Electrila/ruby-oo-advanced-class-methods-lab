b3a8a825b88cb4b7a183341c929ea6553edf5637
megann <megann.elizabeth.king@gmail.com>
2021-07-16 17:01:38 -0400

    commit tree fea5f419f38e22f88f2b581842c777bd9582e159

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,59 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find { |song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by { |song| song.name }
+  end
+
+  def self.new_from_filename(filename)
+    info = filename.split(" - ")
+    artist_name = info[0]
+    song_name = info[1].gsub(".mp3", "")
+    song = self.new
+    song.artist_name = artist_name
+    song.name = song_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song_info = self.new_from_filename(filename)
+    song_info.save
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
   def self.all
     @@all
   end
